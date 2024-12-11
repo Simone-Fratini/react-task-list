313ca4d6ef139e0086bdc32e2ba9d4996eef9f07
Simone-Fratini <simone@fratini.com>
2024-12-11 18:05:59 +0100

    Refactor App component and add header with task manager title; introduce task data structure

---
+++ src/components/headerComponent.jsx
@@ -0,0 +1,10 @@
+
+function headerComponent() {
+    return(
+        <header className="py-3 px-3 sf-bggreen">
+            <h1 className="sf-title">Task Manager</h1>
+        </header>
+    );
+}
+
+export default headerComponent;
